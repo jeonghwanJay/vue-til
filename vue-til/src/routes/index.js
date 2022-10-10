0e09ca8cc06524bcf5c68e5f4b9ee4deb851bdee
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-10 22:06:44 +0900

    edit : App, index(in routes folder)

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -3,4 +3,15 @@ import Vue from "vue"
 
 Vue.use(VueRouter)
 
-export default new VueRouter()
+export default new VueRouter({
+	routes: [
+		{
+			path: "/login",
+			component: () => import("@/views/LoginPage.vue"),
+		},
+		{
+			path: "/signup",
+			component: () => import("@/views/SignupPage.vue"),
+		},
+	],
+})
