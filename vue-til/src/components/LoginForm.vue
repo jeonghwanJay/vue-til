63e15f0c75577c3342607a84b78993a20487a888
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-13 23:27:56 +0900

    add : utils folder, validation.js edit : index(in api folder), LoginForm, SignupForm

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -2,26 +2,54 @@
 	<form v-on:submit.prevent="submitForm">
 		<div>
 			<label for="username">id:</label>
-			<input id="username" type="text" />
+			<input id="username" type="text" v-model="username" />
 		</div>
 		<div>
 			<label for="password">pw:</label>
-			<input id="password" type="new-password" />
+			<input id="password" type="password" v-model="password" />
 		</div>
-		<button type="submit">로그인</button>
+		<button type="submit" v-bind:disabled="!isUsernameValid || !password">
+			로그인
+		</button>
+		<p>{{ logmessage }}</p>
 	</form>
 </template>
 
 <script>
+import { loginUser } from "@/api/index"
+import { validateEmail } from "@/utils/validation"
 export default {
 	data() {
 		return {
 			username: "",
 			password: "",
+			logmessage: "",
 		}
 	},
+	computed: {
+		isUsernameValid() {
+			return validateEmail(this.username)
+		},
+	},
 	methods: {
-		submitForm() {},
+		async submitForm() {
+			try {
+				const userData = {
+					username: this.username,
+					password: this.password,
+				}
+				const { data } = await loginUser(userData)
+				this.logmessage = `${data.user.username}님 환영합니다.`
+				// this.initForm()
+			} catch (error) {
+				this.logmessage = error.response.data
+			} finally {
+				this.initForm()
+			}
+		},
+		initForm() {
+			;(this.username = ""), (this.password = "")
+		},
 	},
 }
 </script>
