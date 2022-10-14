0afff0f0d0a27f6bd613f0149659bc2657fbe125
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-14 23:20:49 +0900

    add : LoginSpinner.vue, PostListItem.vue edit : MainPage

---
+++ vue-til/src/components/posts/PostListItem.vue
@@ -0,0 +1,28 @@
+<template>
+	<ul>
+		<li>
+			<div class="post-title">
+				{{ postItem.title }}
+			</div>
+			<div class="post-contents">
+				{{ postItem.contents }}
+			</div>
+			<div class="post-time">
+				{{ postItem.createdAt }}
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		postItem: {
+			type: Object,
+			required: true,
+		},
+	},
+}
+</script>
+
+<style></style>
--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,26 +2,37 @@
 	<div>
 		<div class="main list-container contents">
 			<h1 class="page-header">Today I Learned</h1>
-			<ul>
-				<li v-for="postItem in postItems" v-bind:key="postItem._id">
-					{{ postItem.title }}
-				</li>
-			</ul>
+			<div v-if="!isLoading"><LoginSpinner></LoginSpinner></div>
+			<PostListItem
+				v-else
+				v-for="postItem in postItems"
+				v-bind:key="postItem._id"
+				v-bind:postItem="postItem"
+			></PostListItem>
 		</div>
 	</div>
 </template>
 
 <script>
 import { fetchPosts } from "@/api/index"
+import PostListItem from "@/components/posts/PostListItem.vue"
+import LoginSpinner from "@/components/common/LoginSpinner.vue"
 export default {
+	components: {
+		PostListItem,
+		LoginSpinner,
+	},
 	data() {
 		return {
 			postItems: [],
+			isLoading: false,
 		}
 	},
 	methods: {
 		async fetchData() {
+			this.isLoading = false
 			const { data } = await fetchPosts()
+			this.isLoading = true
 			console.log(data.posts)
 			this.postItems = data.posts
 		},
