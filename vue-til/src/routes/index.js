0b732ba990efbe968aa81a7835aef1c24494715e
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-17 21:14:19 +0900

    edit : AppHeader, index(in routes folder, in store folder)

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,9 +1,10 @@
 import VueRouter from "vue-router"
 import Vue from "vue"
+import store from "@/store/index"
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: "history",
 	routes: [
 		{
@@ -21,14 +22,17 @@ export default new VueRouter({
 		{
 			path: "/add",
 			component: () => import("@/views/PostAddPage.vue"),
+			meta: { auth: true },
 		},
 		{
 			path: "/main",
 			component: () => import("@/views/MainPage.vue"),
+			meta: { auth: true },
 		},
 		{
 			path: "/posts/:id",
 			component: () => import("@/views/PostEditPage.vue"),
+			meta: { auth: true },
 		},
 		{
 			path: "*",
@@ -36,3 +40,12 @@ export default new VueRouter({
 		},
 	],
 })
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		console.log("인증이 필요합니다.")
+		next("/login")
+		return
+	}
+	next()
+})
+export default router
